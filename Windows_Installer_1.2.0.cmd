e81274b1ab7f23c09b2a1fd72aa243896ba312d3
Ñatick Plays <99545540+NatickYT@users.noreply.github.com>
2022-03-25 07:04:37 +0000

    Update and rename Windows_Installer_1.1.0.cmd to Windows_Installer_1.2.0.cmd

---
+++ Windows_Installer_1.2.0.cmd
@@ -0,0 +1,12 @@
+title BetterMC Installer 1.2.0
+Start Forge Installation
+title Forge Installation
+java -jar forge-1.8.9-11.15.1.2318-1.8.9-installer.jar
+Start JEI Installation
+title JEI Installation
+cd ..
+move Downloads\Mods\jei_1.8.9-2.28.18.187.jar .AppData\Roaming\.minecraft\mods
+Done
+title Complete
+pause
+exit
