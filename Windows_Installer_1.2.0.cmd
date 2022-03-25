707e970d42f44093f3d168d93cf19c56a36f288e
Ñatick Plays <99545540+NatickYT@users.noreply.github.com>
2022-03-25 22:28:11 +0000

    Update Windows_Installer_1.2.0.cmd

--- Windows_Installer_1.2.0.cmd
+++ Windows_Installer_1.2.0.cmd
@@ -1,4 +1,4 @@
-title BetterMC Installer 1.2.0
+title BetterMC Installer 1.3.0
 Start Forge Installation
 title Forge Installation
 java -jar forge-1.8.9-11.15.1.2318-1.8.9-installer.jar
@@ -6,6 +6,26 @@ Start JEI Installation
 title JEI Installation
 cd ..
 move Downloads\Mods\jei_1.8.9-2.28.18.187.jar .AppData\Roaming\.minecraft\mods
+Done
+Start Mouse Tweaks Installation
+title Mouse Tweaks Installation
+move Downloads\Mods\MouseTweaks-2.6.2-mc1.8.9.jar .AppData\Roadming\.minecraft\mods
+Done
+Start JER Installation
+title JER Installation
+move Downloads\Mods\JustEnoughResources-1.8.9-0.4.0.23.jar .AppData\Roadming\.minecraft\mods
+Done
+Start JER API Installation
+title JER API Installation
+move Downloads\Mods\JustEnoughResources-1.8.9-0.4.0.23-api.jar .AppData\Roadming\.minecraft\mods
+Done
+Start JER Sources Installation
+title JER Sources Installation
+move Downloads\Mods\MouseTweaks-2.6.2-mc1.8.9-sources.jar .AppData\Roadming\.minecraft\mods
+Done
+Start JER Deobf Installation
+title JER Deobf Installation
+move Downloads\Mods\MouseTweaks-2.6.2-mc1.8.9-deobf.jar .AppData\Roadming\.minecraft\mods
 Done
 title Complete
 pause
